721a7e9f653f6a76884e55829a84659bf409c11c
Franky <frankykolondam@gmail.com>
2025-01-26 14:26:23 +0700

    chore: update netlify configuration and response utility

--- netlify/functions/utils/response.js
+++ netlify/functions/utils/response.js
@@ -1,20 +1,23 @@
 function createResponse(statusCode, data, options = {}) {
-    const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(',') || []
-    const origin = options.origin || '*'
-    
-    const headers = {
-      'Access-Control-Allow-Origin': allowedOrigins.includes(origin) ? origin : allowedOrigins[0],
-      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-      'Content-Type': 'application/json',
-      ...options.headers
-    }
+  // Allow all origins in development mode
+  const isDev = process.env.NODE_ENV === 'development'
+  const allowedOrigins = isDev ? ['*'] : (process.env.ALLOWED_ORIGINS?.split(',') || [])
+  const origin = options.origin || '*'
   
-    return {
-      statusCode,
-      headers,
-      body: JSON.stringify(data)
-    }
+  const headers = {
+    'Access-Control-Allow-Origin': isDev ? '*' : (allowedOrigins.includes(origin) ? origin : allowedOrigins[0]),
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+    'Access-Control-Allow-Credentials': 'true',
+    'Content-Type': 'application/json',
+    ...options.headers
   }
-  
-  module.exports = { createResponse }
+
+  return {
+    statusCode,
+    headers,
+    body: JSON.stringify(data)
+  }
+}
+
+module.exports = { createResponse }
